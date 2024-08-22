f356dee74180573f7fc6edd8cef06b7a2b3f9f56
Pc-Emi <emiliodelpin@gmail.com>
2024-08-21 22:51:02 -0300

    Initial commit

---
+++ src/main/java/com/ChallengeGalicia/PathsStations/PathsStationsApplication.java
@@ -0,0 +1,13 @@
+package com.ChallengeGalicia.PathsStations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathsStationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathsStationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChallengeGalicia/PathsStations/PathsStationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChallengeGalicia.PathsStations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathsStationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
